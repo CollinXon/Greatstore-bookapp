eb6a0dedafaef293182d72e6f32610f3cea09723
Collins Ennah <ennahcollins@gmail.com>
2024-07-14 00:17:57 +0100

    Update Vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -12,7 +11,5 @@ export default defineConfig({
       },
     },
   },
-
-
   plugins: [react()],
-})
+});
